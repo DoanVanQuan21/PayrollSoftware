5fd3f96eca8165f70fb971f575d05511ccde25f2
quan.doan <dvquan210502@gmail.com>
2024-07-06 07:41:09 +0700

    update

---
+++ src/Core/PayrollSoftware.Core/Mvvms/ManagementRegionViewModel.cs
@@ -0,0 +1,21 @@
+﻿namespace PayrollSoftware.Core.Mvvms
+{
+    public abstract class ManagementRegionViewModel : BaseRegionViewModel
+    {
+        private int pageIndex;
+        private int row;
+
+        public int PageIndex
+        { get => pageIndex; set { SetProperty(ref pageIndex, value); UpdateRow().GetAwaiter(); } }
+
+        public int Row
+        { get => row; set { SetProperty(ref row, value); UpdateRow().GetAwaiter(); } }
+
+        public List<int> Rows = new() { 5, 10, 15, 20, 25, 30 };
+
+        public virtual Task UpdateRow()
+        {
+            return Task.CompletedTask;
+        }
+    }
+}
---
+++ src/Project/ModuleProjects/PayrollSoftware.ProjectManagement/ViewModels/ProjectViewModel.cs
@@ -0,0 +1,16 @@
+﻿using PayrollSoftware.Core.Models.TaskManagement;
+using PayrollSoftware.Core.Mvvms;
+using System.Collections.ObjectModel;
+
+namespace PayrollSoftware.ProjectManagement.ViewModels
+{
+    internal class ProjectViewModel : ManagementRegionViewModel
+    {
+        public override string Title => "Quản lý dự án";
+        public ObservableCollection<Project> Projects { get; set; }
+        public ProjectViewModel()
+        {
+            Projects = new();
+        }
+    }
+}
